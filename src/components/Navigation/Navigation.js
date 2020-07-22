7a78c3d3b18e2c875363a994d419c62af860ab7d
Hubert Góral <hubert.goral@inspirelabs.pl>
2020-07-22 12:41:08 +0200

    delete uselles elements and add Navigation folder

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+const Navigation = () => (
+  <nav>
+    <span>ELO</span>
+    <ul>
+      <li>articles</li>
+      <li>contact</li>
+    </ul>
+  </nav>
+)
+
+export default Navigation
